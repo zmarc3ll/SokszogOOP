1cbe89b3c3761d39a89ed5bafc35c7efcb240b00
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-10-04 15:16:03 +0200

    haromszog osztály befejezése

--- src/hu/petrik/sokszogoop/Haromszog.java
+++ src/hu/petrik/sokszogoop/Haromszog.java
@@ -3,53 +3,74 @@ package hu.petrik.sokszogoop;
 public class Haromszog extends Sokszog{
     private double b;
     private double c;
-
+    public Haromszog(){
+        super(getVeletlenOldal());
+        this.b= getVeletlenOldal();
+        this.c= getVeletlenOldal();
+        while(!isSzerkesztheto()){
+            super.setA(getVeletlenOldal());
+            this.b=getVeletlenOldal();
+            this.c=getVeletlenOldal();
+        }
+    }
+    private static double getVeletlenOldal() {
+        return Math.random()*10+5;
+    }
     public Haromszog(double a, double b, double c) {
         super(a);
         this.b = b;
         this.c = c;
+        if (!this.isSzerkesztheto()){
+            throw new IllegalArgumentException("A háromszög nem szerkeszthető");
+        }
     }
-
     public double getB() {
         return b;
     }
-
-    public void setB(double b) {
-        this.b = b;
-    }
-
     public double getC() {
         return c;
     }
-
+    public void setB(double b) {
+        this.b = b;
+        if (!this.isSzerkesztheto()){
+            throw new IllegalArgumentException("A háromszög nem szerkeszthető");
+        }
+    }
     public void setC(double c) {
         this.c = c;
+        if (!this.isSzerkesztheto()){
+            throw new IllegalArgumentException("A háromszög nem szerkeszthető");
+        }
     }
-    private boolean isSzerkesztheto() {
-        boolean szerkesztheto =true;
-
-        if ((this.getA() + this.b) <= this.getC() ||
-           (this.getA() + this.getC()) <= this.getB()||
-           (this.getB() + this.getC()) <= this.getA()) {
+    @Override
+    public void setA(double a) {
+        super.setA(a);
+        if (!this.isSzerkesztheto()){
+            throw new IllegalArgumentException("A háromszög nem szerkeszthető");
+        }
+    }
+    private boolean isSzerkesztheto(){
+        boolean szerkesztheto= true;
+        if ((this.getA()+this.b)<=this.getC() ||
+                (this.getA()+this.getC())<=this.getB() ||
+                (this.getB()+this.getC())<= this.getA()){
             szerkesztheto=false;
         }
         return szerkesztheto;
     }
-
-    public double getKerulet() {
+    public double getKerulet(){
         return this.getA()+this.getB()+this.getC();
     }
     private double getS(){
-        return getKerulet() /2;
+        return getKerulet()/2;
     }
-
-    public double getKErulet(){
-        return Math.sqrt(this.getS()*(this.getS()-this.getA())*(this.getS() - this.getB()) * (this.getS() - this.getC()));
+    public double getTerulet(){
+        return Math.sqrt(this.getS()*(this.getS()-this.getA())*(this.getS()-this.getB())*(this.getS()-this.getC()));
     }
-
     @Override
     public String toString() {
-        //TODO: tostring elkészítése
-        return "";
+        return String.format("háromszög: a = %f - b = %f - c = %f - K = %f - T = %f",
+                this.getA(),this.getB(),this.getC(),this.getKerulet(),this.getTerulet());
     }
 }
+
