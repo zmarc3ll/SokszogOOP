a950d1c1d454dd796efa73dd09be7d21bb616897
zmarc3ll <zubermarcellpetrik@gmail.com>
2022-10-04 20:27:15 +0200

    main be felvétel és kiiratas

--- src/hu/petrik/sokszogoop/Main.java
+++ src/hu/petrik/sokszogoop/Main.java
@@ -1,5 +1,8 @@
 package hu.petrik.sokszogoop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
@@ -7,17 +10,18 @@ public class Main {
 
             System.out.println(h);
         }
+        System.out.println();
         for (int i = 0; i < 5; i++) {
             double a = Math.random() * 10 + 5;
             double b = Math.random() * 10 + 5;
 
             System.out.println(new Teglalap(a, b));
         }
+        System.out.println();
         for (int i = 0; i < 5; i++) {
             double a = Math.random() * 10.0 + 5.0;
             double b = Math.random() * 10.0 + 5.0;
             double alfa = Math.random() * 178.0 + 1.0;
-
             System.out.println(new Paralelogramma(a, b, alfa));
         }
     }
