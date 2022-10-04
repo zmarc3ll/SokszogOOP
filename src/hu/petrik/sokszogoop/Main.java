7d194c801f96ec1c642de8e62e03334363ddb8d2
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-10-04 15:22:34 +0200

    parameter nelkuli knstruktor

--- src/hu/petrik/sokszogoop/Main.java
+++ src/hu/petrik/sokszogoop/Main.java
@@ -2,6 +2,14 @@ package hu.petrik.sokszogoop;
 
 public class Main {
     public static void main(String[] args) {
-        
+        for (int i = 0; i < 5; i++) {
+            Haromszog h = new Haromszog();
+            System.out.println(h);
+        }
+        for (int i = 0; i <5 ; i++) {
+            double a = Math.random()*10+5;
+            double b = Math.random()*10+5;
+            System.out.println(new Teglalap(a,b));
+        }
     }
 }
